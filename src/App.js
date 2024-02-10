69da9207bc9d2cc37e055c6ef5c0cfa133c225a1
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-02-10 21:33:48 +0100

    setup init prj mui tailwind

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Button>Kek</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
