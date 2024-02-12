d745ebd4d1188a045663a4ec64308cf8a8036bd3
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-02-12 18:34:13 +0100

    removed imgs

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
-// import Course from './Course';
 import img from '../images/girlHD.png'
-// import text from '../images/sticker.webp'
-// import nickname from '../images/nickname.webp'
+
 
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import Contacts from './Contacts';
@@ -40,7 +38,6 @@ const ShippingCalculator = () => {
     return (
         <>
             <div className='relative bg-yellow-50 flex justify-center'>
-                {/* <img className=' overflow-hidden absolute top-4 right-4 md:right-1/3' src={nickname} alt="nickname" /> */}
                 <div className='absolute top-4 right-4 md:right-1/3 flex flex-col'>
                     <Text size={'20'} text={'D'} />
                     <Text size={'20'} text={'U'} />
@@ -59,7 +56,6 @@ const ShippingCalculator = () => {
                 </div>
             </div >
             <div className="p-8 pb-2 flex flex-col items-center w-full bg-gradient-to-t from-pink-100 via-pink-200 to-pink-300">
-                {/* <img src={text} width='400px' alt="Калькулятор доставки" /> */}
                 <Text text={'Калькулятор доставки'} />
                 <p className="mb-4 text-center font-medium italic">Прорахуйте вартість доставки<br />вказавши вагу у віконечку <span className='not-italic'>&#x1F60A;</span>
 
