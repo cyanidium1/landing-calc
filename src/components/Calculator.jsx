2a714d67e7317a4c2ac8306cb179b1600d9d0c48
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-02-12 16:26:09 +0100

    styles upd

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -38,12 +38,12 @@ const ShippingCalculator = () => {
         <>
             <div className='relative bg-yellow-50 flex justify-center'>
                 <div className=' min-h-[50vh] w-96' style={{ backgroundImage: `url(${img})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <div className='absolute bottom-[-2px] left-0 w-screen min-h-[50vh] bg-gradient-to-b from-transparent via-transparent to-pink-400'>
+                    <div className='absolute bottom-[-2px] left-0 w-screen min-h-[50vh] bg-gradient-to-b from-transparent via-transparent to-pink-300'>
 
                     </div>
                 </div>
             </div>
-            <div className="p-8 h-[50vh] flex flex-col items-center w-full bg-gradient-to-t from-pink-200 via-pink-300 to-pink-400">
+            <div className="p-8 h-[50vh] flex flex-col items-center w-full bg-gradient-to-t from-pink-100 via-pink-200 to-pink-300">
                 <img src={text} alt="Калькулятор доставки" />
                 <p className="mb-4 text-center font-medium">Прорахуйте вартість доставки<br />вказавши вагу у віконечку &#x1F60A;
 
@@ -56,7 +56,7 @@ const ShippingCalculator = () => {
                         type="number"
                         value={weight}
                         onChange={handleWeightChange}
-                        InputProps={{ className: 'text-gray-700 bg-white' }}
+                        InputProps={{ className: 'text-gray-700 bg-rose-200' }}
                         InputLabelProps={{ className: 'text-gray-700' }}
                         variant="filled"
                     />
@@ -64,7 +64,6 @@ const ShippingCalculator = () => {
                         <InputLabel id="demo-simple-select-label">Тариф</InputLabel>
                         <Select
                             color="secondary"
-
                             labelId="demo-simple-select-label"
                             id="demo-simple-select"
                             value={tarif}
