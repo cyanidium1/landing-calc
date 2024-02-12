c44f170b6177eedf42c8d3b12c4f01c6e98c1b01
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-02-12 16:42:17 +0100

    added nickname

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,6 +3,8 @@ import TextField from '@mui/material/TextField';
 // import Course from './Course';
 import img from '../images/girl.png'
 import text from '../images/sticker.webp'
+import nickname from '../images/nickname.webp'
+
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import Contacts from './Contacts';
 
@@ -37,14 +39,15 @@ const ShippingCalculator = () => {
     return (
         <>
             <div className='relative bg-yellow-50 flex justify-center'>
+                <img className='absolute top-4 right-4' src={nickname} alt="nickname" />
                 <div className=' min-h-[50vh] w-96' style={{ backgroundImage: `url(${img})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     <div className='absolute bottom-[-2px] left-0 w-screen min-h-[50vh] bg-gradient-to-b from-transparent via-transparent to-pink-300'>
 
                     </div>
                 </div>
             </div>
-            <div className="p-8 h-[50vh] flex flex-col items-center w-full bg-gradient-to-t from-pink-100 via-pink-200 to-pink-300">
-                <img src={text} alt="Калькулятор доставки" />
+            <div className="p-8 pb-2 flex flex-col items-center w-full bg-gradient-to-t from-pink-100 via-pink-200 to-pink-300">
+                <img src={text} width='400px' alt="Калькулятор доставки" />
                 <p className="mb-4 text-center font-medium">Прорахуйте вартість доставки<br />вказавши вагу у віконечку &#x1F60A;
 
                 </p>
